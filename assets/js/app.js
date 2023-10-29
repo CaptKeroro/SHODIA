d34cdad252c8acf6664e4e8691eaac04091b77fb
Surya Gempita <95684526+CaptKeroro@users.noreply.github.com>
2023-10-29 13:43:25 +0800

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+function on() {
+  var menuBar = document.getElementById("menuBar");
+
+  var overlay = document.getElementById("overlay");
+
+  menuBar.classList.toggle("menuBarStyle");
+  overlay.classList.toggle("overlayStyle");
+}
+
+function kategoriOn() {
+  var kategori = document.getElementById("kategori");
+
+  kategori.classList.toggle("kategoriStyle");
+}
